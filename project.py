05a130f891232cee087051e3d4a3cebfef7bbdae
Wiktor <email@dsw.edu.pl>
2024-07-09 20:47:22 +0200

    Task1: Dodanie parsowania argumentów

---
+++ project.py
@@ -0,0 +1,16 @@
+import sys
+
+def parse_arguments():
+    if len(sys.argv) != 3:
+        print("Usage: program.exe pathFile1.x pathFile2.y")
+        sys.exit(1)
+
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    return input_file, output_file
+
+if __name__ == "__main__":
+    input_file, output_file = parse_arguments()
+    print(f"Input file: {input_file}")
+    print(f"Output file: {output_file}")
